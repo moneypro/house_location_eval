bcfeb983df012812dc46f30c72e7d6e174fc3e09
Hong Cheng <hcheng17@illinois.edu>
2021-10-05 22:28:03 -0700

    Completed main function and logic.

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import googlemaps
+from googlemaps.directions import directions
+from _datetime import datetime, timezone, timedelta
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+with open("./google_api.secret") as f:
+    google_api_key = f.readline()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+gmaps = googlemaps.Client(key=google_api_key)
+house_location_list = [
+    "22928 NE 15th Pl Sammamish, WA 98074",
+    "4932 125th Ave SE Bellevue, WA 98006",
+    "6009 147th Ave SE Bellevue, WA 98006",
+    "4429 233rd Pl SE Bothell, WA 98021"
+]
+now = datetime.now()
+morning = datetime(now.year, now.month, now.day + 1, 8, 0, 0, tzinfo=timezone(timedelta(hours=-8)))
+afternoon = datetime(now.year, now.month, now.day + 1, 15, 0, 0, tzinfo=timezone(timedelta(hours=-8)))
+# night also represents off hours / weekends
+night = datetime(now.year, now.month, now.day + 1, 21, 0, 0, tzinfo=timezone(timedelta(hours=-8)))
+frequent_places_departure_time = {
+    "Jing Mei": ("HR2H+WJ Bellevue, Washington", [morning, afternoon]),
+    "Asian Family Market Bellevue": ("JVG3+P9 Bellevue, Washington", [morning, afternoon, night]),
+    "Bellevue Square": ("JQ8W+79 Bellevue, Washington", [night]),
+}
+for house_location in house_location_list:
+    print("House location: {}".format(house_location))
+    for destination_name, destination_data in frequent_places_departure_time.items():
+        destination_location, time_to_departure_list = destination_data
+        print("Destination: {}".format(destination_name))
+        for time_to_departure in time_to_departure_list:
+            directions_result = directions(client=gmaps, origin=house_location, destination=destination_location,
+                                           departure_time=time_to_departure)
+            if len(directions_result) == 0:
+                print("When departure at {}：00, no data is available.".format(time_to_departure.hour))
+            print("When departure at {}:00, takes {} and the distance is {}.".format(
+                time_to_departure.hour,
+                directions_result[0]['legs'][0]['duration_in_traffic']['text'],
+                directions_result[0]['legs'][0]['distance']['text']))
+    print("----------------")
